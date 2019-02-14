57d96b935482e21ab16c56d454ebc16e62480af4
Jan Piotrowski <piotrowski+git@gmail.com>
2019-02-14 17:48:16 +0100

    implement solution!

--- fake.js
+++ fake.js
@@ -4,4 +4,10 @@ console.log("Trying to fake the shelljs.which() return value...")
 
 // Here we have to manipulate shelljs.which() somehow
 
+const shelljs = require('shelljs')
+var new_which = function() {
+    return "/usr/bin/ruby"
+}
+shelljs.which = new_which
+
 require("./index.js");
